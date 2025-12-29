ec01f95886956cb8657049fa86ea4258a01b321c
neelam.chavan@smartbear.com <neelam.chavan@smartbear.com>
2025-12-29 11:05:49 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-20639.stoplight-dev.com/api/v1/projects/cHJqOjYyNw/spectral.js?branch_slug=main&token=6d20731c-9449-4d75-97bf-a02b8ee41022";
+export default { extends: ruleset };
